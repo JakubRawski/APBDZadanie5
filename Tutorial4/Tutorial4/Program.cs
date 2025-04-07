bf8fadd39a11ec0f5ed6d58575412a433bc82dac
Jakub Rawski <kuba.s.rawski@gmail.com>
2025-04-07 09:45:34 +0200

    dodano 2 testy

---
+++ Tutorial4/Tutorial4/Program.cs
@@ -0,0 +1,10 @@
+namespace Tutorial3;
+using Tutorial3.Models;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+       //EmpDeptSalgradeTests.ShouldReturnAllSalesmen();
+    }
+}
